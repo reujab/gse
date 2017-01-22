a7c1d682daf5bb8ba0ad959d51a95e19f1bedf68
reujab <reujab@gmail.com>
2017-01-22 01:36:56 -0500

    moved search to its own file

--- main.go
+++ main.go
@@ -1,26 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
-	"net/http"
-	"net/url"
 	"os"
 
 	"gopkg.in/urfave/cli.v1"
 )
-
-type extensionQuery struct {
-	Extensions []*struct {
-		Creator     string `json:"creator"`
-		Description string `json:"description"`
-		Name        string `json:"name"`
-		UUID        string `json:"uuid"`
-		ID          int    `json:"pk"`
-	} `json:"extensions"`
-}
 
 type gnomeVersion struct {
 	Major string `xml:"platform"`
@@ -71,45 +58,4 @@ func getGNOMEVersion() string {
 	check(xml.Unmarshal(file, data))
 
 	return fmt.Sprintf("%s.%s.%s", data.Major, data.Minor, data.Patch)
-}
-
-func search(ctx *cli.Context) error {
-	args := ctx.Args()
-
-	if len(args) > 1 {
-		return cli.ShowCommandHelp(ctx, "search")
-	}
-
-	query, _ := url.ParseQuery("")
-
-	query.Add("page", ctx.String("page"))
-	query.Add("search", args.First())
-	query.Add("shell_version", getGNOMEVersion())
-
-	res, err := http.Get(baseURL + "/extension-query/?" + query.Encode())
-
-	check(err)
-
-	if res.StatusCode != 200 {
-		return cli.NewExitError("404", 1)
-	}
-
-	body, err := ioutil.ReadAll(res.Body)
-
-	check(err)
-
-	extensions := new(extensionQuery)
-
-	check(json.Unmarshal(body, extensions))
-
-	// TODO: prettier output
-	for i, extension := range extensions.Extensions {
-		if i != 0 {
-			fmt.Println()
-		}
-
-		fmt.Printf("%s%s%s - %s - %s\n", bold, extension.Name, normal, extension.UUID, extension.Description)
-	}
-
-	return nil
 }
---
+++ search.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"gopkg.in/urfave/cli.v1"
+)
+
+type extensionQuery struct {
+	Extensions []*struct {
+		Description string `json:"description"`
+		Name        string `json:"name"`
+		UUID        string `json:"uuid"`
+	} `json:"extensions"`
+}
+
+func search(ctx *cli.Context) error {
+	args := ctx.Args()
+
+	if len(args) > 1 {
+		return cli.ShowCommandHelp(ctx, "search")
+	}
+
+	query, _ := url.ParseQuery("")
+
+	query.Add("page", ctx.String("page"))
+	query.Add("search", args.First())
+	query.Add("shell_version", getGNOMEVersion())
+
+	res, err := http.Get(baseURL + "/extension-query/?" + query.Encode())
+
+	check(err)
+
+	if res.StatusCode != 200 {
+		return cli.NewExitError("404", 1)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	check(err)
+
+	extensions := new(extensionQuery)
+
+	check(json.Unmarshal(body, extensions))
+
+	// TODO: prettier output
+	for i, extension := range extensions.Extensions {
+		if i != 0 {
+			fmt.Println()
+		}
+
+		fmt.Printf("%s%s%s - %s - %s\n", bold, extension.Name, normal, extension.UUID, extension.Description)
+	}
+
+	return nil
+}
