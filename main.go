5dffcb6d7da1a5671f518bc71c090513a519c7d7
reujab <reujab@gmail.com>
2017-08-04 20:15:49 -0400

    removed GNOMEVersion

--- main.go
+++ main.go
@@ -1,36 +1,3 @@
 package gse
 
-import (
-	"encoding/xml"
-	"fmt"
-	"io/ioutil"
-)
-
-// GNOMEVersion defines a version of the GNOME Shell.
-type GNOMEVersion struct {
-	Major string `xml:"platform"`
-	Minor string `xml:"minor"`
-	Patch string `xml:"micro"`
-}
-
-// GetGNOMEVersion returns the current version of GNOME.
-func GetGNOMEVersion() (*GNOMEVersion, error) {
-	file, err := ioutil.ReadFile("/usr/share/gnome/gnome-version.xml")
-	if err != nil {
-		return nil, err
-	}
-
-	version := new(GNOMEVersion)
-	err = xml.Unmarshal(file, version)
-	if err != nil {
-		return nil, err
-	}
-
-	return version, nil
-}
-
-func (version *GNOMEVersion) String() string {
-	return fmt.Sprintf("%s.%s.%s", version.Major, version.Minor, version.Patch)
-}
-
 const baseURL = "https://extensions.gnome.org"
